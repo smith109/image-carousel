52c23363df34def3b4516ceff97259e7748c8e8f
smith109 <devon.smith017@gmail.com>
2025-08-14 22:50:16 -0500

    Add a function to update the carousel

---
+++ script.js
@@ -0,0 +1,25 @@
+const createSlideShow = (carousel) => {
+  const navCircles = carousel.querySelectorAll('.navigation-circles > span');
+  const slidesContainer = carousel.querySelector('.slides-container');
+  const slidesNodeList = carousel.querySelectorAll('.slide');
+  const totalSlides = slidesNodeList.length;
+  let currentIndex = 0;
+
+  const setActiveNavCircle = (index) => {
+    const activeNavCircle = carousel.querySelector('.active');
+    activeNavCircle.classList.remove('active');
+    navCircles[index].classList.add('active');
+  };
+
+  const goToSlide = (index) => {
+    slidesContainer.style.transform = `translateX(-${index * 100}%)`;
+  };
+
+  const updateCarousel = (index) => {
+    setActiveNavCircle(index);
+    goToSlide(index);
+    currentIndex = index;
+  };
+
+  return { updateCarousel };
+};
